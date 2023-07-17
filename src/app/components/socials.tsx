d87bcf267517f22b6d8826969b3569c0d835be94
Alex <alexandre.elkhoury@kardinal.ai>
2023-07-17 23:35:15 +0200

    Added socials

---
+++ src/app/components/socials.tsx
@@ -0,0 +1,8 @@
+export default function Socials() {
+    return (
+        <div className="flex justify-center items-center text-sky-600 text-lg">
+            <a target="_blank" href="https://www.linkedin.com/in/alexandre-el-khoury/" className="mr-8 hover:cursor-pointer hover:underline">[LinkedIn]</a>
+            <a target="_blank" href="https://github.com/m3talux" className="hover:cursor-pointer hover:underline">[Github]</a>
+        </div>
+    )
+}
--- src/app/sections/main.tsx
+++ src/app/sections/main.tsx
@@ -1,4 +1,5 @@
 import Avatar from "@/app/components/avatar";
+import Socials from "@/app/components/socials";
 
 export function Main() {
     return (
@@ -6,6 +7,8 @@ export function Main() {
             <Avatar/>
             <h2 className="text-gray-700 text-center font-bold text-3xl md:text-5xl mt-4 md:mt-16">Hey, my name is Alex</h2>
             <p className="text-gray-600 text-center text-xl md:text-3xl mt-2 md:mt-4">Pleased to meet you!</p>
+            <div className="mt-8"/>
+            <Socials/>
         </section>
     )
 }
