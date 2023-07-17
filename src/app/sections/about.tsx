3dd9591189c37c91c1fc074eacd26f2ebe737bc8
Alex <alexandre.elkhoury@kardinal.ai>
2023-07-17 23:24:22 +0200

    Dynamic years of experience

--- src/app/sections/about.tsx
+++ src/app/sections/about.tsx
@@ -1,9 +1,12 @@
 export function About() {
+    const currentYear = new Date().getFullYear();
+    const experienceYears = currentYear - 2018;
+
     return (
         <section className="bg-sky-600 flex flex-col lg:flex-row items-center content-center p-8 md:p-16">
             <img src="/programmer.svg" className="max-w-xl w-1/2 lg:w-1/3" alt="Alexandre El Khoury | Software"/>
             <p className="leading-8 lg:leading-10 text-center mt-8 lg:text-left lg:ml-16 text-xl md:text-2xl">
-                Experienced software engineer with over 5 years of expertise in the software industry, specializing
+                Experienced software engineer with over {experienceYears} years of expertise in the software industry, specializing
                 primarily in back-end development. Skilled in cross-platform development and possessing a solid
                 foundation in front-end development due to previous experience as a full-stack developer. A passion for
                 coding since the age of 17, with a consistent track record of adapting rapidly to changes and embracing
