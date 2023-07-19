0c890941273bdf25175439bbb7bb18e95f66f1e2
Alex <alexandre.elkhoury@kardinal.ai>
2023-07-19 23:16:15 +0200

    Fixed metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,14 +14,14 @@ export const metadata: Metadata = {
     ],
     openGraph: {
         type: 'website',
-        url: 'https://alexandrekhoury.dev',
+        url: '/',
         locale: 'en-US',
         title: 'Alexandre El Khoury | Software Engineer',
         description: 'Experienced software engineer with 5+ years of expertise in back-end development. Skilled in cross-platform and front-end development. Passionate about coding since age 17, adaptable to new technologies. Exceptionally productive, delivering high-quality and clean code efficiently.',
         images: [
             {
-                url: 'https://alexandrekhoury.dev/svg/avatar.svg',
-                alt: 'Alexandre El Khoury',
+                url: '/avatar.svg',
+                alt: 'Alexandre El Khoury | Software Engineer',
             },
         ],
     },
@@ -30,18 +30,14 @@ export const metadata: Metadata = {
         description: 'Experienced software engineer with 5+ years of expertise in back-end development. Skilled in cross-platform and front-end development. Passionate about coding since age 17, adaptable to new technologies. Exceptionally productive, delivering high-quality and clean code efficiently.',
         images: [
             {
-                url: 'https://alexandrekhoury.dev/svg/avatar.svg',
-                alt: 'Alexandre El Khoury',
+                url: '/avatar.svg',
+                alt: 'Alexandre El Khoury | Software Engineer',
             },
         ],
     }
 }
 
-export default function RootLayout({
-                                       children,
-                                   }: {
-    children: React.ReactNode
-}) {
+export default function RootLayout({children}: { children: React.ReactNode }) {
     return (
         <html lang="en">
         <body className={inter.className}>{children}</body>
