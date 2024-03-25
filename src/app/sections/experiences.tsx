e0fc97b6fd9acd33c8a567b8ede52ebde58b5bb6
Alex <alexandre.elkhoury@kardinal.ai>
2024-03-25 23:28:58 +0100

    UI Update: Experiences

--- src/app/sections/experiences.tsx
+++ src/app/sections/experiences.tsx
@@ -2,45 +2,70 @@
 
 import React, {useState} from "react";
 import experiences from "@/app/content/experiences.json"
-import Experience from "@/app/components/experience";
+import Image from "next/image";
 
 export default function Experiences() {
-    const [index, setIndex] = useState(0);
-
     return (
-        <section className="p-2 lg:p-8 text-white my-32">
-            <div
-                className="w-full h-[80px] bg-sky-950 rounded-t-3xl flex flex-row justify-between items-center px-8 border-b-[1px] border-gray-900 border-opacity-30">
-                <h2 className="text-xl lg:text-3xl font-bold">My Experience</h2>
-                <div className="flex flex-row justify-center items-center">
-                    <div className="w-4 h-4 lg:w-6 lg:h-6 rounded-full bg-yellow-600 mr-4"/>
-                    <div className="w-4 h-4 lg:w-6 lg:h-6 rounded-full bg-green-600 mr-4"/>
-                    <div className="w-4 h-4 lg:w-6 lg:h-6 rounded-full bg-red-600"/>
-                </div>
-            </div>
-            <div className="min-h-[1100px] w-full bg-sky-950 rounded-b-3xl flex flex-col xl:flex-row">
-                <div
-                    className="flex xl:flex-col overflow-auto min-w-[200px] flex-row h-full w-full xl:w-[200px] border-r-0 lg:border-r-[1px] border-gray-900 border-opacity-30">
-                    {
-                        experiences.map((exp: IExperience, i: number) => (
-                                <div key={i} onClick={() => setIndex(i)}
-                                     className={`px-4 py-4 lg:py-8 text-md lg:text-xl hover:cursor-pointer hover:bg-sky-900 ${index == i ? 'bg-sky-900' : ''}`}>{exp.companyTitle}
+        <section className="px-4 xl:px-16 my-32 w-full flex flex-col items-center">
+            <h2 className="text-center text-[var(--text-color)] font-bold text-3xl">Discover my journey</h2>
+            <div className="mt-32 max-w-[1400px]">
+                {
+                    experiences.map((e, i) =>
+                        (
+                            <div
+                                className={`flex ${i % 2 != 0 ? 'flex-col xl:flex-row-reverse' : 'flex-col xl:flex-row'} items-center ${i < experiences.length - 1 ? 'mb-32' : 'mb-0'} w-full`}
+                                key={i}>
+                                <div className="flex flex-col items-center xl:w-[800px]">
+                                    <a href={e.companyURL} target="_blank">
+                                        <Image className="object-center xl:w-[500px] w-[200px]" src={e.companyImg}
+                                               alt={e.companyTitle}
+                                               width={500}
+                                               height={100}/>
+                                    </a>
+                                    <h3 className="mt-8 font-bold text-center">{e.title}</h3>
+                                    <div className="mt-2 flex flex-row items-center">
+                                        <p>{e.period}</p>
+                                        {
+                                            i == 0 && <span className="ml-4 relative flex h-3 w-3">
+                                        <span
+                                            className="animate-ping absolute inline-flex h-full w-full rounded-full bg-[var(--primary-color)] opacity-75"></span>
+                                        <span
+                                            className="relative inline-flex rounded-full h-3 w-3 bg-[var(--primary-color)]"></span>
+                                    </span>
+                                        }
+                                    </div>
+                                    {e.kpis.length > 0 &&
+                                        <div
+                                            className="mt-8 border-gray-200 border-[1px] w-[300px] rounded-xl px-8 py-4">
+                                            {
+                                                e.kpis.map((k, i) => (
+                                                    <div
+                                                        className={`${i < e.kpis.length - 1 ? 'mb-4' : 'mb-0'} flex flex-row items-center justify-between`}
+                                                        key={i}>
+                                                        <div className="flex flex-col justify-center items-start">
+                                                            <label>{k.title}</label>
+                                                            <p className="text-xs">{k.description}</p>
+                                                        </div>
+                                                        <h4 className="ml-4 text-3xl text-[var(--primary-color)]">{k.kpi}</h4>
+                                                    </div>
+                                                ))
+                                            }
+                                        </div>
+                                    }
                                 </div>
-                            )
-                        )
-                    }
-                </div>
-                <div className="flex-1 p-8 mb-16">
-                    <Experience companyTitle={experiences[index].companyTitle}
-                                companyImg={experiences[index].companyImg}
-                                companyURL={experiences[index].companyURL}
-                                title={experiences[index].title}
-                                period={experiences[index].period}
-                                type={experiences[index].type}
-                                location={experiences[index].location}
-                                achievements={experiences[index].achievements}
-                                kpis={experiences[index].kpis}/>
-                </div>
+                                <div
+                                    className={`${i % 2 != 0 ? 'xl:mr-32' : 'xl:ml-32'} xl:mt-0 mt-8 text-[var(--accent-color)] grow`}>
+                                    {
+                                        e.achievements.map((a, i) => (
+                                                <p className={`${i < e.achievements.length - 1 ? 'mb-8' : 'mb-0'} text-sm xl:text-base`}
+                                                   key={i}>{a}</p>
+                                            )
+                                        )
+                                    }
+                                </div>
+                            </div>
+                        ))
+                }
             </div>
         </section>
     )
