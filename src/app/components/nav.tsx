0a65234e7b6e28323bea31cdb2fa4c0e97e00473
Alex <alexandre.elkhoury@kardinal.ai>
2024-11-22 22:32:07 +0200

    build fix

--- src/app/components/nav.tsx
+++ src/app/components/nav.tsx
@@ -1,12 +1,13 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Nav() {
     return (
         <nav className="w-screen px-8 md:px-32 py-6 flex justify-between items-center">
-            <a href="/" title="Alexandre El Khoury | Home">
+            <Link href="/" title="Alexandre El Khoury | Home">
                 <Image src="/svg/logo.svg" className="w-[48px] h-auto sm:w-[56px]" alt="Alexandre El Khouy | Logo"
                        width={56} height={56}/>
-            </a>
+            </Link>
             <div className="flex justify-center items-center">
                 <a href="#contact" className="text-[var(--text-color)] mr-8 hover:cursor-pointer"
                    title="Let's Chat!">Contact</a>
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,13 +1,15 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function NotFound() {
     return (
         <div className="h-screen w-screen flex flex-col justify-center items-center">
             <Image src="/svg/404.svg" alt="404 - Not Found" style={{width: '80%', height: 'auto'}} width={800}
                    height={900} className="max-w-[600px]"/>
-            <a className="bg-[var(--primary-color)]  rounded-lg p-4 text-xl md:text-2xl font-bold hover:cursor-pointer mt-8 text-white"
-               href="/" title="Alexandre El Khoury | Home">Back
-                Home</a>
+            <Link
+                className="bg-[var(--primary-color)]  rounded-lg p-4 text-xl md:text-2xl font-bold hover:cursor-pointer mt-8 text-white"
+                href="/" title="Alexandre El Khoury | Home">Back
+                Home</Link>
         </div>
     )
 }
